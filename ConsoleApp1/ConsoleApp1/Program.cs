205947897d55c043d2d82d7795e69ce8e3bf01a6
aamir <wasi3aamir@gmail.com>
2019-01-28 10:50:26 +0600

    Not implement yet

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 3; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    i *= 3;
+                }
+                for (j = 1; j <= 3; j++)
+                {
+                    Console.Write($"{i} ");
+                    i--;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
